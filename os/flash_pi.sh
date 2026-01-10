c4f00a073b7400dadcd7a54865d1588eea3b8dc7
Matt Wheeler <mattw1285@gmail.com>
2026-01-10 15:23:16 +0000

    download working

--- os/flash_pi.sh
+++ os/flash_pi.sh
@@ -1,11 +1,11 @@
-~!/bin/bash
+#!/bin/bash
 
 # --------------------
 # A script to flash my preffered OS version to a drive.
 # - Reproducible Setup
 # - Headless Server Setup
 # - SSH Enabled From The Go
-# - Debian By Default
+# - Pi OS Lite By Default
 # --------------------
 
 
@@ -13,21 +13,41 @@ set -euo pipefail
 echo "Let the flashing begin (My clothes are still on!)..."
 
 
-# harcoded url for default image
+# harcoded defaults
 IMAGE_URL="https://downloads.raspberrypi.com/raspios_lite_arm64/images/raspios_lite_arm64-2025-12-04/2025-12-04-raspios-trixie-arm64-lite.img.xz"
+IMAGE_FILE="/tmp/pi_os.img"
+IMAGE_XZ="$IMAGE_FILE.xz"
 
 
 # promprt for drive if not provided
-if [[$# -ge 1 ]]; then
-    DEVICE ="$1"
+if [[ $# -ge 1 ]]; then
+    DEVICE="$1"
 else
-    read -rp "Enter the traget device:" DEVICE
+    lsblk
+    read -rp "Enter the target device ('a' for abort):" DEVICE
+fi
+if [[ "$DEVICE" == "a" ]]; then
+    echo "Aborted!"
+    exit 1
 fi
 
 
+
 # confirm drive erasure
-read -rp "Are you sure you want to continue? (y/n): " CONFIRM
+read -rp "$DEVICE will be overwriten. Are you sure you want to continue? (y/n):" CONFIRM
 if [[ "$CONFIRM" != "y" ]]; then
     echo "Aborted!"
     exit 1
 fi
+
+
+# obtain image
+if [[ ! -f "$IMAGE_XZ" ]]; then
+    echo "Downloading image..."
+    curl -L "$IMAGE_URL" -o "$IMAGE_XZ"
+else
+    echo "Local image found, skipping download."
+fi
+
+
+echo "Flashing image..."
