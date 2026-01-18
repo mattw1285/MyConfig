0fca9be1b7aaf7b146abdea7ea742eb5dba040b1
Matt Wheeler <mattw1285@gmail.com>
2026-01-18 15:14:51 +0000

    working setup choice

--- os_configs/pi_config.py
+++ os_configs/pi_config.py
@@ -4,14 +4,31 @@ Created on Sat Jan 17 20:18:20 2026
 
 @author: Matt Wheeler
 """
-
+## Default Libraries ##
 from pathlib import Path
+import sys
 
-path = Path(__file__).parent
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python3 my_file.py <mnt_location>")
+mnt_path = Path(sys.argv[1])
+
+
+path = Path(__file__).parent /'cloud-init'
 setups = [
-    x for x in path.iterdir('user-data') 
-    if x.name != 'base-file']
+    x for x in (path /'user-data').iterdir()
+    if x.name != 'base-file'
+]
 print('Choices for setup config:')
 for x in setups:
-    print(f'- {x.name}')
+    print(f'- {x.stem}')
 config = input('Pick a config: ')
+if config not in [x.stem for x in setups]:
+    print('Not a valid settup!')
+    sys.exit(1)
+
+
+if (path /'network-config'/config).exists():
+    pass
+elif (path /'meta-data'/config).exists():
+    pass
