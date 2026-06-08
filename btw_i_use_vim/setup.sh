aebac8465ba1d7e818136af635bbf683cdc97134
Matt Wheeler <mattw1285@gmail.com>
2026-06-08 19:36:29 +0100

    added python executor to setup.sh

--- btw_i_use_vim/setup.sh
+++ btw_i_use_vim/setup.sh
@@ -3,11 +3,11 @@
 SCRIPT_PATH="$( readlink -f "$BASH_SOURCE[0]" )"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 SCRIPT_DIR="$( cd "$SCRIPT_DIR" && pwd)"
-
 echo "MyConfig is located at: '$SCRIPT_DIR'"
 
 echo "We will now beign setting up your user..."
 
 sudo apt install python3 -y
+python3 "$SCRIPT_DIR/helper_scripts/dot_files.py"
 
 
