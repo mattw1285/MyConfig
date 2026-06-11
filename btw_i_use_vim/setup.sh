1811060c1e092b14ca076b2b1877bff00d3c15ad
Matt Wheeler <mattw1285@gmail.com>
2026-06-11 19:53:49 +0100

    finished setup.sh, added in vim-plug install

--- btw_i_use_vim/setup.sh
+++ btw_i_use_vim/setup.sh
@@ -7,7 +7,18 @@ echo "MyConfig is located at: '$SCRIPT_DIR'"
 
 echo "We will now beign setting up your user..."
 
-sudo apt install python3 -y
 python3 "$SCRIPT_DIR/helper_scripts/dot_files.py"
 
+echo "Setting up vim..."
+rm -rf ~/.vim
+mkdir -p ~/.vim/autoload
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugClean! +PlugInstall +PlugUpdate +qall
+echo "vim setup!"
+
+echo "Adding one final alias..."
 echo "alias dev='source $SCRIPT_DIR/dev_py.sh'" >> ~/.bash_aliases
+
+echo "Setup done, we will now reload the shell!"
+source ~/.bashrc
