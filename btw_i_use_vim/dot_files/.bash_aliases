4417f0291058756155d772b31352d2f7f5cca429
Matt Wheeler <mattw1285@gmail.com>
2026-06-14 19:43:20 +0100

    added path input to dev

--- btw_i_use_vim/dot_files/.bash_aliases
+++ btw_i_use_vim/dot_files/.bash_aliases
@@ -4,12 +4,12 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias home='clear && cd ~'
 
-## --- Git --- ##
+## --- Dev Niceness --- ##
 alias ga='git add .'
 alias gc='git commit -m' 
 alias gs='git status'
 alias gl="git log -n 8 --pretty=format:'%C(auto)%h %C(magenta)%ad %C(reset)%s %C(cyan)[%an]' --date=short"
+alias cat='batcat'
 
 ## --- System & Utility --- ##
 alias update='sudo apt update -y && sudo apt full-upgrade -y'
-alias cat='batcat'
--- btw_i_use_vim/helper_scripts/dev.sh
+++ btw_i_use_vim/helper_scripts/dev.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 SESSION="dev"
+START_DIR=$(realpath "${1:-.}")
 
 if tmux has-session -t $SESSION 2>/dev/null; then
     echo "Tmux session '$SESSION' already exists"
 else
-    tmux new-session -s $SESSION -d
-    tmux split-window -v -t $SESSION:0
+    tmux new-session -s $SESSION -d -c "$START_DIR"
+    tmux split-window -v -t $SESSION:0 -c "$START_DIR"
     tmux resize-pane -t $SESSION:0.0 -y 75%
     sleep 0.2
     tmux send-keys -t $SESSION:0.0 "vim" C-m
