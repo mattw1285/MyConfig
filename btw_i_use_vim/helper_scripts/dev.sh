53a40462325c4ed89360cc2ead3dae84c0402cf7
Matt Wheeler <mattw1285@gmail.com>
2026-06-14 16:13:00 +0100

    dev vim startup bug fix

--- btw_i_use_vim/helper_scripts/dev.sh
+++ btw_i_use_vim/helper_scripts/dev.sh
@@ -5,10 +5,12 @@ SESSION="dev"
 if tmux has-session -t $SESSION 2>/dev/null; then
     echo "Tmux session '$SESSION' already exists"
 else
-    tmux new-session -s $SESSION -d "vim"
+    tmux new-session -s $SESSION -d
     tmux split-window -v -t $SESSION:0
     tmux resize-pane -t $SESSION:0.0 -y 75%
+    sleep 0.1
     tmux send-keys -t $SESSION:0.0 "vim" C-m
-    tmux select-pane -t $SESSION:0.0
+    sleep 0.1
+    tmux select-pane -t $SESSION:0.1
     tmux attach -t $SESSION
 fi
