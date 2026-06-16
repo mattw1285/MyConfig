2b504596bc4b4b2014c453af0629fc674318d37c
Matt Wheeler <mattw1285@gmail.com>
2026-06-16 20:58:53 +0100

    changed dev to h split

--- btw_i_use_vim/helper_scripts/dev.sh
+++ btw_i_use_vim/helper_scripts/dev.sh
@@ -7,13 +7,13 @@ if tmux has-session -t $SESSION 2>/dev/null; then
     echo "Tmux session '$SESSION' already exists"
 else
     tmux new-session -s $SESSION -d -c "$START_DIR" "bash"
-    tmux split-window -v -t $SESSION:0 -c "$START_DIR" "bash"
-    tmux resize-pane -t $SESSION:0.0 -y 75%
-    tmux select-pane -t $SESSION:0.0
+    tmux split-window -h -t $SESSION:0 -c "$START_DIR" "bash"
+    tmux resize-pane -t $SESSION:0.0 -x 20%
+    tmux select-pane -t $SESSION:0.1
     sleep 0.2
     CMD=" \
         sleep 0.1; \
-        tmux send-keys -t $SESSION:0.0 vim C-m; \
+        tmux send-keys -t $SESSION:0.1 vim C-m; \
         tmux set-hook -u $SESSION client-attached \ 
     "
     tmux set-hook -t $SESSION client-attached "run-shell '$CMD'"
